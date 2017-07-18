74a748a3633ee660cb15224f9b95c772363dbaa3
Michael Englert <michi.eng@gmail.com>
2017-07-18 10:29:58 +0200

    removed tags removed agent dependencies

--- ECommerce-Docker/demo-build.sh
+++ ECommerce-Docker/demo-build.sh
@@ -1,21 +1,10 @@
 # VARIABLES
-ZIP_MACHINE_AGENT=/Users/rbolton/Documents/Code/appd/agents/MachineAgent-4.2.5.0.zip
-WEB_SERVER_AGENT=/Users/rbolton/Documents/Code/appd/agents/appdynamics-sdk-native-nativeWebServer-64bit-linux-4.2.5.0.tar.gz
-APP_SERVER_AGENT=/Users/rbolton/Documents/Code/appd/agents/AppServerAgent-4.2.5.0.zip
-LOCAL_TOMCAT=/Users/rbolton/Documents/Code/appd/agents/apache-tomcat-7.0.70.tar.gz
-AGENT_VERSION=4.2.5.0
+LOCAL_TOMCAT=
 
-(cp ${APP_SERVER_AGENT} ECommerce-Tomcat/AppServerAgent.zip)
 (cp ${LOCAL_TOMCAT} ECommerce-Tomcat/apache-tomcat.tar.gz)
-echo "Copied Agent and Tomcat for ECommerce-Tomcat"
+echo "Copied Tomcat for ECommerce-Tomcat"
 
-cp ${WEB_SERVER_AGENT} ECommerce-LBR/webserver_agent.tar.gz
-echo "Copied Agent for ECommerce-LBR"
-
-(cp ${ZIP_MACHINE_AGENT} ECommerce-MachineAgent/MachineAgent.zip)
-echo "Copied machine agent to ECommerce-MachineAgent"
-
-#copy and build 
+#copy and build
 (cd ../ECommerce-Java/ && gradle war uberjar)
 (cd ../)
 
@@ -32,8 +21,6 @@ echo "Copied machine agent to ECommerce-MachineAgent"
 (cp ../ECommerce-Java/zips.sql ECommerce-Tomcat/)
 echo "Files copied over"
 
-DATE=`date +%y.%m.%d`
-VERSION="${AGENT_VERSION}-${DATE}"
-cd ECommerce-LBR && docker build --no-cache -t appddemo/ecommerce-lbr:$VERSION .
-cd ../ECommerce-Tomcat && docker build --no-cache -t appddemo/ecommerce-tomcat:$VERSION .
-cd ../ECommerce-MachineAgent && docker build --no-cache -t appddemo/machineagent-analytics:$VERSION .
+cd ECommerce-LBR && docker build --no-cache -t appddemo/ecommerce-lbr .
+cd ../ECommerce-Tomcat && docker build --no-cache -t appddemo/ecommerce-tomcat .
+cd ../ECommerce-MachineAgent && docker build --no-cache -t appddemo/machineagent-analytics .
