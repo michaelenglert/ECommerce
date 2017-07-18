46743b95b6fb39cd33a33c2563edb6f43ff62cf8
Michael Englert <michi.eng@gmail.com>
2017-07-18 10:27:08 +0200

    removed tag and fixed oracle naming clash

--- ECommerce-Docker/demo-main-images.sh
+++ ECommerce-Docker/demo-main-images.sh
@@ -3,20 +3,17 @@ ORACLE_DB_RPM=
 ORACLE_JAVA=
 
 #### Copy files over
-cp ${ORACLE_DB_RPM} ./ECommerce-Oracle
+cp ${ORACLE_DB_RPM} ./ECommerce-Oracle/oracle-xe-11.2.0-1.0.x86_64.rpm
 cp ${ORACLE_JAVA} ./ECommerce-Java/jdk-linux-x64.rpm
 
 #### Build images
-DATE=`date +%y.%m.%d`
 cd ECommerce-Oracle
-docker build -t appddemo/ecommerce-oracle:${DATE} .
+docker build -t appddemo/ecommerce-oracle .
 cd ../ECommerce-MySQL
-docker build -t appddemo/ecommerce-mysql:${DATE} .
+docker build -t appddemo/ecommerce-mysql .
 cd ../ECommerce-Java
-docker build -t appddemo/ecommerce-java:${DATE} .
+docker build -t appddemo/ecommerce-java .
 cd ../ECommerce-ActiveMQ
-docker build -t appddemo/ecommerce-activemq:${DATE} .
+docker build -t appddemo/ecommerce-activemq .
 cd ../ECommerce-Uninstrumented
-docker build -t appddemo/ecommerce-uninstrumented:${DATE} .
-
-
+docker build -t appddemo/ecommerce-uninstrumented .
